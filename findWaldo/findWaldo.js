8efb6233d0c3571270986dca613bf9d7d7abb1dc
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-07-27 13:16:05 +0000

    findWaldo assignment: using forEach instead of for loop

--- findWaldo/findWaldo.js
+++ findWaldo/findWaldo.js
@@ -1,14 +1,8 @@
  // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(currentVal,index) {
+    if (currentVal === "Waldo") {
+      found(index)
     }
-  }
+  })
 }
-
-function actionWhenFound(index) {
-  console.log(`Found Waldo at index ${index}!`);
-}
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);

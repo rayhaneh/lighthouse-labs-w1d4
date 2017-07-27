ace8705f343c9d4dab73f36b3d793cbab772e187
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-07-27 15:22:25 +0000

    Updating the sort function - adding the second part of the assignment

--- sort/sort.js
+++ sort/sort.js
@@ -4,3 +4,32 @@ var result = array.sort(function(a,b){
     return a - b
 })
 console.log(result)
+console.log()
+
+
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+
+var sortedStudents = students.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  } else {
+    if (a.age < b.age) return 1;
+  }
+  return 0;
+});
+
+
+console.log(sortedStudents)
